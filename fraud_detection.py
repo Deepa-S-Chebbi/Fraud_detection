c75cd7ddfc8bf83390e9ac2f01ff824cd2eccac3
Deepa S Chebbi <149763585+Deepa-S-Chebbi@users.noreply.github.com>
2025-09-15 21:04:40 +0530

    Add files via upload

---
+++ fraud_detection.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("fraud_detection_Pipeline.pkl")
+st.title("Fraud Detection  App")
+
+st.markdown(" Please enter the transaction details below:")
+
+st.divider()
+
+transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT", "DEPOSIT"])
+
+amount = st.number_input(" Amount", min_value=0.0, value=1000.0)
+
+oldbalanceOrg = st.number_input("Old Balance of Origin Account", min_value=0.0, value=10000.0)
+
+newbalanceOrig = st.number_input("New Balance of Origin Account", min_value=0.0, value=9000.0)
+
+oldbalanceDest = st.number_input("Old Balance of Destination Account", min_value=0.0, value=0.0)
+newbalanceDest = st.number_input("New Balance of Destination Account", min_value=0.0, value=0.0)
+
+if st.button("Predict Fraud"):
+    input_data = pd.DataFrame({
+        "type": [transaction_type],
+        "amount": [amount],
+        "oldbalanceOrg": [oldbalanceOrg],
+        "newbalanceOrig": [newbalanceOrig],
+        "oldbalanceDest": [oldbalanceDest],
+        "newbalanceDest": [newbalanceDest]
+    })
+
+    prediction = model.predict(input_data)[0]
+
+    st.header("Prediction Result : '{int(prediction)}'")
+   
+
+    if prediction == 1:
+        st.error(f"Fraudulent Transaction Detected! (Probability: {prediction:.2f})")
+    else:
+        st.success(f"Transaction is Legitimate. (Probability: {1 - prediction:.2f})")
